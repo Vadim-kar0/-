7634248da2b9959b1dcc6d174977abeb6bf61cbb
вадим <funpalka1@gmail.com>
2023-04-11 22:00:21 +0400

    seminar_2start

--- 2_part_massivi/Program.cs
+++ 2_part_massivi/Program.cs
@@ -2,7 +2,7 @@
 
 //Первая запись массива и поиск максимального значения 
 
-/* int Max(int arg1, int arg2, int arg3)
+int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -21,11 +21,11 @@ int maxi = Max(
 
 Console.WriteLine(maxi);
 
- */
+
 
 //Вторая запись массива и поиск чего-то того что пока я не знаю
 
-/* 
+
 int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
 
 int n = array.Length;
@@ -43,14 +43,14 @@ while (index < n)
     }
     index++;
     //index = index + 1;
-} */
+}
 
 
 
 //Третья запись массива с использованием 
 //функций (методов) по заполнению и печати 
 
-/* void FillArray(int[] collection)
+void FillArray(int[] collection)
 {
     int Length = collection.Length;
     int index = 0;
@@ -108,7 +108,7 @@ PrintArray(array);
 Console.WriteLine();
 
 int pos = IndexOf(array, 444);
-Console.WriteLine(pos); */
+Console.WriteLine(pos);
 
 
 
--- 2_seminar/Program.cs
+++ 2_seminar/Program.cs
@@ -1,2 +1,104 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Начало 
+
+//Условные выражения 
+// > - болше 
+// >= - больше лтбо равно 
+// < - мнешье 
+// <= - меньше либо равно
+// == - равно 
+// != - Не равно 
+//&& - логическое "И" (должны выполнятся все условия)
+/* int x = 11;
+if (x > 5 && x < 10)
+{
+    Console.WriteLine("Win!");
+
+}
+else
+{
+    Console.WriteLine("Win, but another");
+}
+ */
+
+//|| - логическое "ИЛИ" (должны выполнятся все условия)
+/* int x = 11;
+if (x == 5 || x < 10)
+{
+    Console.WriteLine("Win!");
+
+}
+else
+{
+    Console.WriteLine("Win, but another");
+}
+
+
+ //! - логическое "НЕ" (если было тру станет фалс и наоборот)
+int x = 11;
+if (!(x > 5))
+// {
+//     Console.WriteLine("Win!");
+
+// }
+// else
+// {
+//     Console.WriteLine("Win, but another");
+// }
+ 
+
+
+
+//  int randomNumber = new Random().Next(10, 20); // от 10 до 19 включительно
+//  Console.WriteLine(randomNumber);
+
+
+
+/* Операции с числами 
+% - остаток от деления 
+4 % / == 0
+5 % 2 == 1 */
+
+//Console.WriteLine(5 % 2);
+
+// / - целая часть от деления ( для целых чисел) 
+//или просто деление (для дробных чисел)
+//4 / 2 == 2
+//5 / 2 == 2
+
+// Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int number = new Random().Next(10, 100); // от 10 включительно до 100 НЕ включительно
+// Console.WriteLine("Случайное число: " + number);
+// int first = number / 10; // 45 / 10 == 4
+// int second = number % 10; // 45 % 10 == 5
+// if (first > second)
+// {
+//     Console.WriteLine("Наибольшая цифра числа: " + first);
+// }
+// if (second > first)
+// {
+//     Console.WriteLine("Наибольшая цифра числа: " + second);
+// }
+// if (first == second)
+// {
+//     Console.WriteLine("Цифры числа равны");
+
+// Задача 1. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+
+// 782 -> 72
+
+// 918 -> 98
+
+// int number = new Random().Next(100, 1000); 
+// Console.WriteLine("Случайное число: " + number);
+// int first = number / 100;
+// int second = number % 10;
+// Console.WriteLine("Число без средней цифры");
+// Console.Write(first);
+// Console.Write(second); 
