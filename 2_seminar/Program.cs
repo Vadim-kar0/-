ce34e431a1be8be09f7b779dddc743debe91c398
вадим <funpalka1@gmail.com>
2023-04-11 22:42:14 +0400

    seminar_2finish

--- 2_seminar/Program.cs
+++ 2_seminar/Program.cs
@@ -102,3 +102,60 @@ if (!(x > 5))
 // Console.WriteLine("Число без средней цифры");
 // Console.Write(first);
 // Console.Write(second); 
+
+
+
+// Задача 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число 
+//не кратно числу первому, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно
+
+//  Console.WriteLine("Введите первое число");
+// string text_a = Console.ReadLine();
+// int a = Convert.ToInt32(text_a);
+// Console.WriteLine("Введите второе число");
+// string text_b = Console.ReadLine();
+// int b = Convert.ToInt32(text_b);
+// int sf = a % b;
+// if( sf == 0)
+// {
+//    Console.Write(" кратно ");
+// }
+
+// if(sf != 0)
+// {
+//    Console.Write("Не кратно, остаток от деления : ");
+//    Console.Write(sf);
+// } 
+
+
+// Задача 3. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14  ->  нет
+// 46  ->  нет
+// 161 ->  да
+
+
+
+Console.WriteLine("Введите число");
+string text_a = Console.ReadLine();
+int a = Convert.ToInt32(text_a);
+
+if(a % 7 == 0 && a % 23 ==0)
+{
+     Console.Write("число кратно 7 и 23");
+}
+else
+{
+    Console.Write("число не кратно 7 и 23");
+}
+
+
+
+
+
+
+
+
+
